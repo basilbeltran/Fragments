5fed1174a690a83f57265670842646e59f253796
Basil Beltran <Basil.Beltran@gmail.com>
2015-07-21 12:09:10 -0600

    Chapter 8

--- app/src/main/java/com/just8/apps/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/just8/apps/criminalintent/CrimeActivity.java
@@ -21,6 +21,4 @@ public class CrimeActivity extends FragmentActivity {
                     .commit(); }
     }
 
-
-
 }
--- app/src/main/java/com/just8/apps/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/just8/apps/criminalintent/CrimeFragment.java
@@ -8,7 +8,13 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+
+import java.util.Date;
 
 
 /**
@@ -18,6 +24,8 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     public CrimeFragment() {  } // Required empty public constructor
 
@@ -39,6 +47,19 @@ public class CrimeFragment extends Fragment {
             }
             public void beforeTextChanged(CharSequence c, int start, int count, int after) {}
             public void afterTextChanged(Editable c) {}
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
         });
 
         return v;
